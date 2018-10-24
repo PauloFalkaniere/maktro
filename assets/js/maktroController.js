ac368c2926eade90fd940215fea73b8f90f2bac0
Paulo Falkaniere <paulofalkaniere@gmail.com>
2018-10-23 23:35:40 -0300

    add psw

--- assets/js/maktroController.js
+++ assets/js/maktroController.js
@@ -7,6 +7,152 @@
 //   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 // }
 
+openPhotoSwipeGalery = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [
+      {
+          src: 'assets/image/galeria/02.png',
+          w: 964,
+          h: 1024
+      },
+      {
+          src: 'assets/image/galeria/peca.png',
+          w: 1024,
+          h: 683
+      },
+      {
+          src: 'assets/image/galeria/imagemDisel.png',
+          w: 1024,
+          h: 964
+      },
+      {
+          src: 'assets/image/galeria/maktro01.jpg',
+          w: 1024,
+          h: 683
+      },
+      {
+          src: 'assets/image/galeria/banner01.jpg',
+          w: 1024,
+          h: 683
+      },
+      {
+          src: 'assets/image/galeria/empresasLocacao.jpg',
+          w: 1024,
+          h: 683
+      }
+    ];
+
+    // define options (if needed)
+    var options = {
+			 // history & focus options are disabled on CodePen
+      	history: false,
+      	focus: false,
+
+        showAnimationDuration: 0,
+        hideAnimationDuration: 0
+
+    };
+
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+};
+
+openPhotoSwipeDiesel = function() {
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [
+        {
+            src: 'assets/image/galeria/imagemDisel.png',
+            w: 1024,
+            h: 964
+        },
+        {
+            src: 'assets/image/galeria/maktro01.jpg',
+            w: 1024,
+            h: 683
+        }
+    ];
+
+    // define options (if needed)
+    var options = {
+			 // history & focus options are disabled on CodePen
+      	history: false,
+      	focus: false,
+
+        showAnimationDuration: 0,
+        hideAnimationDuration: 0
+
+    };
+
+    var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+  };
+openPhotoSwipeLocacoes = function() {
+      var pswpElement = document.querySelectorAll('.pswp')[0];
+
+      // build items array
+      var items = [
+          {
+              src: 'assets/image/galeria/banner01.jpg',
+              w: 1024,
+              h: 683
+          },
+          {
+              src: 'assets/image/galeria/empresasLocacao.jpg',
+              w: 1024,
+              h: 683
+          }
+      ];
+
+      // define options (if needed)
+      var options = {
+  			 // history & focus options are disabled on CodePen
+        	history: false,
+        	focus: false,
+
+          showAnimationDuration: 0,
+          hideAnimationDuration: 0
+
+      };
+
+      var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+      gallery.init();
+    };
+openPhotoSwipePecas = function() {
+        var pswpElement = document.querySelectorAll('.pswp')[0];
+
+        // build items array
+        var items = [
+            {
+                src: 'assets/image/galeria/02.png',
+                w: 964,
+                h: 1024
+            },
+            {
+                src: 'assets/image/galeria/peca.png',
+                w: 1024,
+                h: 683
+            }
+        ];
+
+        // define options (if needed)
+        var options = {
+    			 // history & focus options are disabled on CodePen
+          	history: false,
+          	focus: false,
+
+            showAnimationDuration: 0,
+            hideAnimationDuration: 0
+
+        };
+
+        var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+        gallery.init();
+      };
+
 
 function valida(){
   field = $("#email").val();
