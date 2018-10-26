c97cb2e827ca90a5a4f8933db876ab317ac1861b
Paulo Falkaniere <paulofalkaniere@gmail.com>
2018-10-25 22:53:44 -0300

    altera banner, add phpmailer

--- email.php
+++ email.php
@@ -1,39 +1,67 @@
+
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+$for = 'paulofalkaniere@gmail.com';
+$for = 'vendas@maktrodiesel.com.br';
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 
-if (isset($_POST['token'])) {
-  $for = 'vendas@maktrodiesel.com.br';
-  $post = extract($_POST);
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $content = $_POST['content'];
-  $subject = "Contato Via Site";
+// if (isset($_POST['token'])) {
 
-  $mensagem = "<strong>Nome:  </strong>".$name;
-  $mensagem .= "<br><strong>Email: </strong>".$email;
-  $mensagem .= "<br><strong>Mensagem: </strong>".$_POST['content'];
+    // $post = extract($_POST);
+    // $name = $_POST['name'];
+    // $email = $_POST['email'];
+    // $content = $_POST['content'];
+    // $mensagem = "<strong>Nome:  </strong>".$name;
+    // $mensagem = "<br><strong>Email:  </strong>".$email;
+    // $mensagem .= "<br><strong>Mensagem: </strong>".$content;
 
-//5 – agora inserimos as codificações corretas e  tudo mais.
-  $headers =  "Content-Type:text/html; charset=UTF-8\n";
-  $headers .= "From:  www.maktrodiesel.com.br\n"; //Vai ser //mostrado que  o email partiu deste email e seguido do nome
-  $headers .= "X-Sender:  <site@maktrodiesel.com.br>\n"; //email do servidor //que enviou
-  $headers .= "X-Mailer: PHP  v".phpversion()."\n";
-  $headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
-  $headers .= "Return-Path:  <site@maktrodiesel.com.br>\n"; //caso a msg //seja respondida vai para  este email.
-  $headers .= "MIME-Version: 1.0\n";
+
+        $name = "paulo";
+        $email = "paulofalkaniere";
+        $content = "aaaaaaaaaaaaaaaaaaaaaa";
+        $mensagem = "<strong>Nome:  </strong>".$name;
+        $mensagem = "<br><strong>Email:  </strong>".$email;
+        $mensagem .= "<br><strong>Mensagem: </strong>".$content;
+
   try {
-    mail($for, $subject, $mensagem, $headers);  //função que faz o envio do email.
-    $response_array['status'] = 'success';
-  } catch (\Exception $e) {
+
+     //Server settings
+     $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+     $mail->isSMTP();                                      // Set mailer to use SMTP
+     $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+     $mail->SMTPAuth = true;                               // Enable SMTP authentication
+     $mail->Username = 'user@example.com';                 // SMTP username
+     $mail->Password = 'secret';                           // SMTP password
+     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+     $mail->Port = 587;                                    // TCP port to connect to
+
+     //Recipients
+     $mail->setFrom($for, 'Site');
+     $mail->addAddress($for, 'Vendas Maktro');
+     //Content
+     $mail->isHTML(true);                                  // Set email format to HTML
+     $mail->Subject = 'Contato Via Site';
+     $mail->Body    = $mensagem;
+
+     $mail->send();
+
+     $response_array['status'] = 'success';
+  } catch (Exception $e) {
+
     $response_array['status'] = 'error';
-    $response_array['message'] = $e->getMessage();
+    $response_array['message'] = $mail->ErrorInfo;
   }
-
-
-
-} else{
-  $response_array['status'] = 'error';
-}
+//
+// } else{
+//   $response_array['status'] = 'error';
+// }
 
 
 echo json_encode($response_array);
+
+
+
  ?>
