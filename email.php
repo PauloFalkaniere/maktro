d524881e017c72fd8e3408982d766adff2970870
Paulo Falkaniere <paulofalkaniere@gmail.com>
2018-10-26 12:06:35 -0300

    envio de email via PHPMailer

--- email.php
+++ email.php
@@ -2,41 +2,33 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-$for = 'paulofalkaniere@gmail.com';
-$for = 'vendas@maktrodiesel.com.br';
 //Load Composer's autoloader
 require 'vendor/autoload.php';
+
+$for = 'vendas@maktrodiesel.com.br';
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 
-// if (isset($_POST['token'])) {
+if (isset($_POST['token'])) {
 
-    // $post = extract($_POST);
-    // $name = $_POST['name'];
-    // $email = $_POST['email'];
-    // $content = $_POST['content'];
-    // $mensagem = "<strong>Nome:  </strong>".$name;
-    // $mensagem = "<br><strong>Email:  </strong>".$email;
-    // $mensagem .= "<br><strong>Mensagem: </strong>".$content;
-
-
-        $name = "paulo";
-        $email = "paulofalkaniere";
-        $content = "aaaaaaaaaaaaaaaaaaaaaa";
-        $mensagem = "<strong>Nome:  </strong>".$name;
-        $mensagem = "<br><strong>Email:  </strong>".$email;
-        $mensagem .= "<br><strong>Mensagem: </strong>".$content;
+    $post = extract($_POST);
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $content = $_POST['content'];
+    $mensagem = "<strong>Nome:  </strong>".$name;
+    $mensagem .= "<br><strong>Email:  </strong>".$email;
+    $mensagem .= "<br><strong>Mensagem: </strong>".$content;
 
   try {
 
      //Server settings
-     $mail->SMTPDebug = 2;                                 // Enable verbose debug output
-     $mail->isSMTP();                                      // Set mailer to use SMTP
-     $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
-     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-     $mail->Username = 'user@example.com';                 // SMTP username
-     $mail->Password = 'secret';                           // SMTP password
+     // $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+     $mail->isSMTP();
+     $mail->Host = 'mail.maktrodiesel.com.br ';
+     $mail->Port = 587;
+     $mail->SMTPAuth = true;
+     $mail->Username = 'contatosite@maktrodiesel.com.br';
+     $mail->Password = '_Cb0_hx.6cE*';          // SMTP password
      $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-     $mail->Port = 587;                                    // TCP port to connect to
 
      //Recipients
      $mail->setFrom($for, 'Site');
@@ -54,14 +46,11 @@ $mail = new PHPMailer(true);                              // Passing `true` enab
     $response_array['status'] = 'error';
     $response_array['message'] = $mail->ErrorInfo;
   }
-//
-// } else{
-//   $response_array['status'] = 'error';
-// }
+} else{
+  $response_array['status'] = 'error';
+  $response_array['message'] = 'Token and POST are missing';
 
+}
 
-echo json_encode($response_array);
-
-
-
+  echo json_encode($response_array);
  ?>
